a710b4a6d9ec83f9621cb2e6a77d1799dd1db5c1
Adrian.Mendoza <adrian.mendoza@jalasoft.com>
2022-08-11 19:42:18 -0400

    AM: Setup Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
+import Theme from '@/theme';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <Theme>
+    <App />
+  </Theme>
+);
