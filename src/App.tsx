a09fbc20b0751a61d408e3945a73cb566870fdbf
Adrian.Mendoza <adrian.mendoza@jalasoft.com>
2022-08-11 04:07:53 -0400

    AM: Base implementation for react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from '@/common/Router';
 
-const App: FC = () => <div>Home</div>;
+const App: FC = () => (
+  <BrowserRouter>
+    <AppRouter />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/common/Router/styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from '@mui/styles';
+
+export default makeStyles({
+  active: {
+    color: 'green',
+    background: '#121212',
+    borderRadius: '0.5rem',
+    textDecoration: 'none'
+  },
+  noActive: {
+    color: 'white',
+    background: 'none',
+    textDecoration: 'none'
+  }
+});
