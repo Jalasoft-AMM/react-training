4f6125ae3f8121c1e84a6e4f8f6568d868a2c351
Adrian.Mendoza <adrian.mendoza@jalasoft.com>
2022-08-17 13:39:33 -0400

    AM: Error handling

--- src/redux/actions/character.action.ts
+++ src/redux/actions/character.action.ts
@@ -7,22 +7,29 @@ import { getAll, getSingleCharacter } from '@/services/character.service';
 export const characterActions = characterSlice.actions;
 
 export const fetchCharacters =
-  (): ThunkAction<void, RootState, unknown, AnyAction> => (dispatch, getState) => {
-    dispatch(characterActions.setLoading(true));
+  (): ThunkAction<void, RootState, unknown, AnyAction> =>
+  (dispatch, getState): void => {
     if (getState().character.characters.length === 0) {
-      getAll().then((response: Character[]) => {
-        setTimeout(() => {
-          dispatch(characterActions.setCharacters(response));
-          dispatch(characterActions.setLoading(false));
-        }, 2000);
-      });
+      dispatch(characterActions.startLoading());
+      getAll()
+        .then((response: Character[]) => {
+          setTimeout(() => {
+            dispatch(characterActions.setCharacters(response));
+          }, 2000);
+        })
+        .catch((error) => {
+          setTimeout(() => {
+            dispatch(characterActions.setError(error.message));
+          }, 2000);
+        });
     }
   };
 
 export const fetchSingleCharacter =
   (id: number): ThunkAction<void, RootState, unknown, AnyAction> =>
-  (dispatch) => {
-    getSingleCharacter(id).then((response: Character) => {
-      dispatch(characterActions.setSingleCharacter(response));
-    });
+  (dispatch, getState): void => {
+    if (getState().character.singleCharacter.id !== id)
+      getSingleCharacter(id).then((response: Character) => {
+        dispatch(characterActions.setSingleCharacter(response));
+      });
   };
--- src/redux/slices/character.slice.ts
+++ src/redux/slices/character.slice.ts
@@ -5,14 +5,14 @@ interface CharacterState {
   characters: Character[];
   singleCharacter: Character;
   loading: boolean;
-  errors: any;
+  error: string;
 }
 
 const initialState: CharacterState = {
   characters: [],
   singleCharacter: CharacterEmptyState,
   loading: false,
-  errors: null
+  error: ''
 };
 
 const characterSlice = createSlice({
@@ -20,14 +20,19 @@ const characterSlice = createSlice({
   initialState,
 
   reducers: {
+    startLoading: (state) => {
+      state.loading = true;
+    },
+    setError: (state, { payload }: PayloadAction<string>) => {
+      state.error = payload;
+      state.loading = false;
+    },
     setCharacters: (state, { payload }: PayloadAction<Character[]>) => {
       state.characters = payload;
+      state.loading = false;
     },
     setSingleCharacter: (state, { payload }: PayloadAction<Character>) => {
       state.singleCharacter = payload;
-    },
-    setLoading: (state, { payload }: PayloadAction<boolean>) => {
-      state.loading = payload;
     }
   }
 });
